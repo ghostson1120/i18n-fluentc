c6b4ed7d926d436b3005b834a880d80b7bd1df99
ghostson1120 <ghostson1120@outlook.com>
2023-03-01 06:29:10 +0200

    vue2 demo

---
+++ example/vue2/src/components/AboutSection.vue
@@ -0,0 +1,60 @@
+<template>
+  <section id="about">
+    <v-container fluid>
+      <v-row align="center" justify="center">
+        <v-col cols="10">
+          <v-row align="center" justify="center">
+            <v-col cols="12" md="7">
+              <h1 class="font-weight-light display-2">{{ $t('about') }}</h1>
+              <v-row>
+                <v-col cols="12" class="d-flex align-center">
+                  <v-img
+                    src="@/assets/img/icon1.svg"
+                    max-width="60px"
+                    class="mr-4"
+                  />
+                  <p class="text-justify">
+                    {{ $t('single-point-for-content') }}
+                  </p>
+                </v-col>
+                <v-col cols="12" class="d-flex align-center">
+                  <v-img
+                    src="@/assets/img/icon2.svg"
+                    max-width="60px"
+                    class="mr-4"
+                  />
+                  <p class="text-justify">
+                    {{ $t('web-mobile-and-more') }}
+                  </p>
+                </v-col>
+                <v-col cols="12" class="d-flex align-center">
+                  <v-img
+                    src="@/assets/img/icon3.svg"
+                    max-width="60px"
+                    class="mr-4"
+                  />
+                  <p class="text-justify">
+                    {{ $t('brand-strategy') }}
+                  </p>
+                </v-col>
+              </v-row>
+            </v-col>
+            <v-col cols="12" md="5" class="d-none d-md-flex">
+              <v-img
+                src="@/assets/img/ill1.svg"
+                class="d-block ml-auto mr-auto"
+                max-width="400px"
+              />
+            </v-col>
+          </v-row>
+        </v-col>
+      </v-row>
+    </v-container>
+  </section>
+</template>
+
+<style scoped>
+#about {
+  background-color: #f4f7f5;
+}
+</style>
