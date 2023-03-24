1be3334cf0bcf347d181bb777660f167a57a6320
unknown <ghostson1120@outlook.com>
2023-03-24 15:22:04 +0200

    add Angular Demo

--- example/angular/src/app/app.module.ts
+++ example/angular/src/app/app.module.ts
@@ -2,13 +2,16 @@ import { APP_INITIALIZER, NgModule, LOCALE_ID } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
 import { I18NEXT_SERVICE, I18NextModule, I18NextLoadResult, ITranslationService, defaultInterpolationFormat  } from 'angular-i18next';
 import LanguageDetector from 'i18next-browser-languagedetector';
-import HttpApi from 'i18next-http-backend';
+import FluentCApi from '../../../../';
 
 import { AppComponent } from './app.component';
 
 const i18nextOptions = {
   debug: true, // set debug?
   fallbackLng: 'en',
+  backend: {
+    environmentId: '3d6933d1-1fdf-4390-aa5a-0a75e04be3d6'
+  },
   interpolation: {
     format: I18NextModule.interpolationFormat(defaultInterpolationFormat)
   }
@@ -17,7 +20,7 @@ const i18nextOptions = {
 export function appInit(i18next: ITranslationService) {
   return () => {
     let promise: Promise<I18NextLoadResult> = i18next
-      .use(HttpApi)
+      .use(FluentCApi)
       .use<any>(LanguageDetector)
       .init(i18nextOptions);
     return promise;
