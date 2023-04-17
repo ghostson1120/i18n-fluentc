52d49c9ec1438f6a02e72eea038964bad96009ff
unknown <ghostson1120@outlook.com>
2023-04-17 17:29:05 +0300

    update nextjs demo

--- example/NextJS/config/index.js
+++ example/NextJS/config/index.js
@@ -1,5 +1,5 @@
 const dev = process.env.NODE_ENV !== 'production'
 
-export const server = dev ? 'http://localhost:3000' : 'http://localhost:3000';
+export const server = location.origin;
 // https://maxbase.org
 
