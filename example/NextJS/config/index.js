7e652b8f79b9f57499c0bc2caaf6c97b04bacb7a
unknown <ghostson1120@outlook.com>
2023-04-17 18:24:31 +0300

    update demos

--- example/NextJS/config/index.js
+++ example/NextJS/config/index.js
@@ -1,5 +1,5 @@
 const dev = process.env.NODE_ENV !== 'production'
 
-export const server = dev ? 'http://localhost:3000' : 'https://fluentc-demo-next.netlify.app/';
+export const server = dev ? 'http://localhost:3000' : 'https://fluentc-demo-next.netlify.app';
 // https://maxbase.org
 
